115f11d82cb2a95d8e4e5afedf2d8408f6c12691
khadija <khadijalahmar@gmail.com>
2021-09-23 11:39:05 +0100

    create_databas_configuration

--- app/config/config.php
+++ app/config/config.php
@@ -3,13 +3,13 @@
   define('DB_HOST', 'localhost');
   define('DB_USER', 'root'); #username phpmyadmin
   define('DB_PASS', ''); # password phpmyadmin
-  define('DB_NAME', 'gestion_pharmacie'); #database name
+  define('DB_NAME', 'portfolio'); #database name
 
   // App Root
   define('APPROOT', dirname(dirname(__FILE__)));
   // define('APPROOT2', dirname(dirname(dirname(__FILE__))));
   // URL Root
-  define('URLROOT', 'http://localhost/projet_fils_rouge');
+  define('URLROOT', 'http://localhost/portfolio');
   // Site Name
   define('SITENAME', ' management');
   // App Version
