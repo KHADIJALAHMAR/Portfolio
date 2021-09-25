4c4c2b2107ccc53eef16ce84515852955173110c
khadija <khadijalahmar@gmail.com>
2021-09-25 22:21:28 +0100

    style_section-about

--- app/views/pages/profile.php
+++ app/views/pages/profile.php
@@ -5,19 +5,19 @@
 
 <!-- add section  about-->
 <section class="about">
-    <div class="container">
-        <div>
-            <img   src="<?php echo URLROOT; ?>/assets/img/img-PRESENTATION.jpg" alt="" height="280">
+    <div class="flex items-center j">
+        <div class="flex-1">
+            <img   src="<?php echo URLROOT; ?>/assets/img/img-PRESENTATION.jpg" alt="" height="300" >
         </div>
-        <div>
+        <div class="flex-1">
             <h1>About <span>As</span></h1>
             <h3>Hello ! I'm Lahmar Khadija</h3>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem ullam, optio placeat accusamus illum cupiditate, quis ducimus iure deserunt nulla esse nesciunt, culpa in molestiae blanditiis eaque voluptatem eveniet facere.</p>
+            <div class="social">
+                    <a href="#"> <img src="" alt=""></a>
+                    <a href="#"><img src="" alt=""></a>
+                    <a href="#"> <img src="" alt=""></a>
         </div>
-        <div>
-            <a href="#"> <img src="" alt="">faceebok</a>
-            <a href="#"><img src="" alt="">instagram</a>
-            <a href="#"> <img src="" alt="">twitter</a>
         </div>
     </div>
 </section>
